c800c9534f0bec1ba0da49f95f7a63ee6a30d021
pedrovaz100 <pedro.vaz.ferreira@gmail.com>
2025-11-10 22:56:13 -0300

    feat(types): adicionar interfaces Checkin e TeamMember

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface Checkin {
+  id: string;
+  mood: number;
+  comment?: string;
+  date: string;
+}
+
+export interface TeamMember {
+  name: string;
+  rm: string;
+  turma: string;
+  github: string;
+  linkedin: string;
+  photo: string;
+}
